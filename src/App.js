44715c1046f6dfdd07c506c2a9c503f58fe3077f
TiiaJusslin <tiia.jusslin@gmail.com>
2020-10-08 11:26:39 +0300

    suomennos tehty

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Olen täällä oppimassa - sen koodaaminen on nopsaa
         </a>
       </header>
     </div>
