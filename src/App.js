ec2802d8f614557834ee67a00e023465c12d090e
TiiaJusslin <tiia.jusslin@gmail.com>
2020-10-12 11:00:56 +0300

    implements adding new tasks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [taskList, setTaskList] = useState(["moi", "terve", "morjes", "howdy"])
+  const [newTaskItem, updateNewTaskItem] = useState("")
+
+  let addNewTaskToList = function (taskItem){
+    let taskListCopy = [...taskList]
+    taskListCopy.push(taskItem)
+    setTaskList(taskListCopy)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Olen täällä oppimassa - sen koodaaminen on nopsaa
-        </a>
-      </header>
-    </div>
-  );
-}
+      <input type="text" value={newTaskItem} onChange={ (e) => updateNewTaskItem(e.target.value)} />
+      <input type="button" value="Lisää tehtävä" onClick={() => addNewTaskToList(newTaskItem)} />
 
+      { taskList.map( (singleTask) => <p>Tehtävän sisältö: {singleTask}</p>)}
+      {/* <button type="button" onClick={() => increaseCounter(counter + 1)}>Laskurin arvo {counter}</button> */}
+    </div>
+  )
+};
 export default App;
